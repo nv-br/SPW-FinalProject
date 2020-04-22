95e87c574381985b0742e696258ae00a1c5d39a6
nv-br <63592024+nv-br@users.noreply.github.com>
2020-04-22 11:12:02 +0100

    Add files via upload

---
+++ blog/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView,
+    search
+)
+from . import views 
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home' ),
+    path('user/<str:username>',UserPostListView.as_view(), name='user-posts' ),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),    
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+     path(r'^search/$', views.search , name ='search'),
+
+    path('about/', views.about, name='blog-about'),
+]
